a669c3f0916b534d27423421f78768af3a52df3e
Cezary Piatek <cezarypiatek@live.com>
2024-12-07 13:56:51 +0100

    Fix issue with missing encoding

---
+++ RtfPipe/RtfPipeModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+
+namespace System.Runtime.CompilerServices
+{
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+  internal sealed class ModuleInitializerAttribute : Attribute
+  {
+  }
+}
+
+namespace RtfPipe
+{
+  internal static class RtfPipeModule
+  {
+    [ModuleInitializer]
+    internal static void Init()
+    {
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+  }
+}
